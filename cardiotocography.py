714c5a4fd67bae5e1d3f7aa0b38efa230914d65c
Javs <javs.doblerodriguez@gmail.com>
2020-06-10 17:59:44 +0200

    Gráfica de balance de clases

--- cardiotocography.py
+++ cardiotocography.py
@@ -3,10 +3,16 @@
 #############################
 #####     LIBRERIAS     #####
 #############################
-import numpy as np
-import pandas as pd
 import pathlib as pl
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Función auxiliar para detener la ejecución del script entre cada apartado
+def stop():
+    input("\nPulse ENTER para continuar\n")
+    
 # Función lectira de 
 def read_data(file_name):
     data = pd.read_excel(pl.Path(__file__).parent / f"datos/{file_name}", skipfooter= 3, sheet_name= 2, header = 0)
@@ -20,7 +26,16 @@ def read_data(file_name):
 
 data_file = 'CTG.xls'
 
+def plot_class_distribution(y_data, class_names):
+    classes, count = np.unique(y_data, return_counts=True)
+    plt.bar(np.arange(classes.size), count)
+    plt.xticks(np.arange(classes.size), class_names)
+    plt.xlabel("Clase a la que pertenece")
+    plt.ylabel("Ocurrencias de la clase")
+    plt.title(f"Balance de clases en el conjunto de datos con {classes.size} clases")
+    plt.show()
+    stop()
+
 X_data, y_fhr, y_nsp = read_data(data_file)
-print(X_data)
-print(y_fhr)
-print(y_nsp)
+plot_class_distribution(y_fhr, ('A', 'B', 'C', 'D', 'SH', 'AD', 'DE', 'LD', 'FS', 'SUSP'))
+plot_class_distribution(y_nsp, ('Normal', 'Suspect', 'Pathologic'))
