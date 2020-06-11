2900c80e7094217c7e27960725c15174b41faa96
Manuel Jesús Núñez Ruiz <manueljesusnunezruiz@gmail.com>
2020-06-11 16:55:59 +0200

    Añadido mapa de calor sobre la correlacion entre las variables

--- cardiotocography.py
+++ cardiotocography.py
@@ -4,7 +4,6 @@
 #####     LIBRERIAS     #####
 #############################
 import pathlib as pl
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -43,8 +42,6 @@ def plot_histogram(data, feature_names):
     plt.show()
     stop()
 
-data_file = 'CTG.xls'
-
 def plot_class_distribution(y_data, class_names):
     classes, count = np.unique(y_data, return_counts=True)
     plt.bar(np.arange(classes.size), count)
@@ -55,7 +52,34 @@ def plot_class_distribution(y_data, class_names):
     plt.show()
     stop()
 
+def plot_features_correlation(data, feature_names):
+    corr = np.corrcoef(data, rowvar= False)
+
+    fig, ax = plt.subplots(figsize=(11,11))
+
+    # Representación la matriz de correlación y asignandole la colorbar
+    cs = ax.imshow(corr)
+    fig.colorbar(cs)
+    ax.set_xticks(np.arange(len(feature_names)))
+    ax.set_yticks(np.arange(len(feature_names)))
+    ax.set_xticklabels(feature_names)
+    ax.set_yticklabels(feature_names)
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    for i in range(len(feature_names)):
+        for j in range(len(feature_names)):
+            text = ax.text(j, i, "%.2f" % corr[i, j], ha="center", va="center", color="w")
+    
+    ax.set_title('Heatmap de la correlación entre característiscas')
+    fig.tight_layout()
+    plt.show()
+    stop()
+
+data_file = 'CTG.xls'
+
 X_data, y_fhr, y_nsp, feature_names = read_data(data_file)
 plot_class_distribution(y_fhr, ('A', 'B', 'C', 'D', 'SH', 'AD', 'DE', 'LD', 'FS', 'SUSP'))
 plot_class_distribution(y_nsp, ('Normal', 'Suspect', 'Pathologic'))
-plot_histogram(X_data, feature_names)
+#plot_histogram(X_data, feature_names)
+plot_features_correlation(X_data, feature_names[:-2])
